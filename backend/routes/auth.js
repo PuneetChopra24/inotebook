8edd5f05e9af18ad396bd6e681d45cc6c90f82dd
Puneet Chopra <pchopra0250@conestogac.on.ca>
2022-03-26 23:37:59 -0400

    Post request to database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const obj = {
-    a: "this",
-    number: 35,
-  };
-  res.send(obj);
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 });
 
 module.exports = router;
